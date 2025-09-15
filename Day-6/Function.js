9aedd91ea94c6ef12a6d18b25a4d77075c616382
NitishChauhan1 <cnitish440@gmail.com>
2025-09-15 15:27:51 +0530

    Bulb Code with html,css,js

---
+++ Day-6/Function.js
@@ -0,0 +1,13 @@
+//Function
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+let result = add(10, 20);
+console.log(result);
+
+//arrow function
+
+const add = (num1, num2) => num1 + num2;
+let result1 = add(10, 20);
+console.log(result1);
--- Function.js
+++
@@ -1,7 +0,0 @@
-//Function
-
-function add(num1, num2) {
-    return num1 + num2;
-}
-let result = add(10, 20);
-console.log(result);
